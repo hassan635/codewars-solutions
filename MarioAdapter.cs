45a30dad8c80dc50638b15e784d7bdf1ed3ae7ce
hassan635 <hassan.a.tariq@gmail.com>
2020-08-25 08:09:36 +1000

    Adapter design pattern implementation

---
+++ MarioAdapter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+namespace codewars_solutions
+{
+    //This sample shows an implementation of Adaptor design pattern
+    public class Target
+    {
+        public int Health { get; set; }
+    }
+    public interface IUnit
+    {
+        void Attack(Target target);
+    }
+
+    public class Marine : IUnit
+    {
+        public void Attack(Target target)
+        {
+            target.Health -= 6;
+        }
+    }
+
+    public class Zealot : IUnit
+    {
+        public void Attack(Target target)
+        {
+            target.Health -= 8;
+        }
+    }
+
+    public class Zergling : IUnit
+    {
+        public void Attack(Target target)
+        {
+            target.Health -= 5;
+        }
+    }
+
+    public class Mario
+    {
+        public int jumpAttack()
+        {
+            Console.WriteLine("Mamamia!");
+            return 3;
+        }
+    }
+
+    public class MarioAdapter : IUnit
+    {
+        private Mario _mario;
+
+        public MarioAdapter(Mario Mario)
+        {
+            _mario = Mario;
+        }
+
+        public void Attack(Target Target)
+        {
+            Target.Health -= _mario.jumpAttack();
+        }
+    }
+}
