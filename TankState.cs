8fb8199c3208d44a1b3d536c65d9c9b5e9296a00
hassan635 <hassan.a.tariq@gmail.com>
2020-08-27 08:06:07 +1000

    Tank State Pattern

---
+++ TankState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace codewars_solutions
+{
+    public interface ITankUnit
+    {
+        ITankUnitState State { get; set; }
+        bool CanMove { get; }
+        int Damage { get; }
+    }
+
+    public interface ITankUnitState
+    {
+        bool CanMove { get; set; }
+        int Damage { get; set; }
+    }
+
+    public class SiegeState : ITankUnitState
+    {
+        public SiegeState()
+        {
+            CanMove = false;
+            Damage = 20;
+        }
+
+        public bool CanMove { get; set; }
+        public int Damage { get; set; }
+    }
+
+    public class TankState : ITankUnitState
+    {
+        public TankState()
+        {
+            CanMove = true;
+            Damage = 5;
+        }
+
+        public bool CanMove { get; set; }
+        public int Damage { get; set; }
+    }
+
+    public class Tank : ITankUnit
+    {
+        public Tank()
+        {
+            State = new TankState();
+        }
+
+        public ITankUnitState State { get; set; }
+
+        public bool CanMove
+        {
+            get { return State.CanMove; }
+        }
+        public int Damage
+        {
+            get { return State.Damage; }
+        }
+    }
+}
